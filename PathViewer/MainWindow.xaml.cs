248547206955a0a2339429bedd511c5b894bc65c
ignacio_sanchez_pb <ignacio@purobrillo.es>
2021-07-08 12:34:21 +0200

    First commit

---
+++ PathViewer/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace PathViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = new PathViewModel();
+            InitializeComponent();
+        }
+
+        private void ZoomResetClick(object sender, RoutedEventArgs e) => (DataContext as PathViewModel).Zoom = 1;
+    }
+}
---
+++ PathViewer/PathViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+
+namespace PathViewer
+{
+    public class PathViewModel : INotifyPropertyChanged
+    {
+        private double zoom = 1.0;
+        private string strokeColor = nameof(Colors.Black);
+        private int strokeThickness = 2;
+        private string fillColor = nameof(Colors.Transparent);
+        private string data = "M 0,90 A 90,90 0 0 0 180,90 M 30,30 A 15,15 0 0 0 60,30 M 30,30 A 15,15 0 0 1 60,30 M 120,30 A 15,15 0 0 0 150,30 M 120,30 A 15,15 0 0 1 150,30";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Data { get => data; set { data = value; OnPropertyChanged(); } }
+
+        public double Zoom { get => zoom; set { zoom = value; OnPropertyChanged(); } }
+
+        public List<string> ColorsList => typeof(Colors).GetProperties().Select(p => p.Name).ToList();
+
+        public List<int> Thicknesses => Enumerable.Range(1, 20).ToList();
+
+        public string StrokeColor { get => strokeColor; set { strokeColor = value; OnPropertyChanged(); } }
+
+        public int StrokeThickness { get => strokeThickness; set { strokeThickness = value; OnPropertyChanged(); } }
+
+        public string FillColor { get => fillColor; set { fillColor = value; OnPropertyChanged(); } }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
